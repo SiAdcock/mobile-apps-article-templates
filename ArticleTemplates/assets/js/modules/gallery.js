d5f2180e275750862e88de1431a5eb66c1427f9b
George Haberis <georgioshaberis@gmail.com>
2016-01-12 15:22:20 +0000

    DES-1 indentation tidy up

--- ArticleTemplates/assets/js/modules/gallery.js
+++ ArticleTemplates/assets/js/modules/gallery.js
@@ -1,120 +1,120 @@
 define([
-	'bean'
-], function (
-	bean
+    'bean'
+], function(
+    bean
 ) {
-	'use strict';
+    'use strict';
 
-	var galleryRows,
-		galleryProps = {
-			minImagesPerRow: 2,
-			maxImagesPerRow: 3
-		};
+    var galleryRows,
+        galleryProps = {
+            minImagesPerRow: 2,
+            maxImagesPerRow: 3
+        };
 
-	function init() {		
-		addImagesToRows();
-		addFlexClass();
-		adjustHeights();
-		showImages();
+    function init() {
+        addImagesToRows();
+        addFlexClass();
+        adjustHeights();
+        showImages();
 
-		bean.on(window, 'resize', window.ThrottleDebounce.debounce(100, false, resizeHandler));
-		bean.on(window, 'scroll', window.ThrottleDebounce.debounce(100, false, scrollHandler));
+        bean.on(window, 'resize', window.ThrottleDebounce.debounce(100, false, resizeHandler));
+        bean.on(window, 'scroll', window.ThrottleDebounce.debounce(100, false, scrollHandler));
     }
 
     function resizeHandler() {
-    	adjustHeights();
-    	showImages();
+        adjustHeights();
+        showImages();
     }
 
-	function scrollHandler() {
-    	showImages();
+    function scrollHandler() {
+        showImages();
     }
 
     function showImages() {
-    	var i, j;
+        var i, j;
 
-    	for (i = 0; i < galleryRows.length; i++) {
-			for (j = 0; j < galleryRows[i].length; j++) {
-				if (!galleryRows[i][j].classList.contains("loaded") && isImageInView(galleryRows[i][j])) {
-					loadImage(galleryRows[i][j]);
-				}
-			}
-		}
+        for (i = 0; i < galleryRows.length; i++) {
+            for (j = 0; j < galleryRows[i].length; j++) {
+                if (!galleryRows[i][j].classList.contains("loaded") && isImageInView(galleryRows[i][j])) {
+                    loadImage(galleryRows[i][j]);
+                }
+            }
+        }
     }
 
     function isImageInView(imageContainer) {
-		var rect = imageContainer.getBoundingClientRect();
+        var rect = imageContainer.getBoundingClientRect();
 
-		return rect.top <= window.innerHeight;
-	}
+        return rect.top <= window.innerHeight;
+    }
 
-	function loadImage(imageContainer) {
-		var img = document.createElement("img");
+    function loadImage(imageContainer) {
+        var img = document.createElement("img");
 
-		img.src = imageContainer.dataset.url;
+        img.src = imageContainer.dataset.url;
 
-		if (!img.complete) {
-			img.onload = onImageLoaded.bind(null, imageContainer);
-		} else {
-			onImageLoaded(imageContainer);
-		}
-	} 
+        if (!img.complete) {
+            img.onload = onImageLoaded.bind(null, imageContainer);
+        } else {
+            onImageLoaded(imageContainer);
+        }
+    }
 
-	function onImageLoaded(imageContainer) {
-		var image = imageContainer.getElementsByClassName("gallery__image")[0];
+    function onImageLoaded(imageContainer) {
+        var image = imageContainer.getElementsByClassName("gallery__image")[0];
 
-		image.style.backgroundImage = "url('" + imageContainer.dataset.url + "')";
+        image.style.backgroundImage = "url('" + imageContainer.dataset.url + "')";
 
-		imageContainer.classList.add("loaded");
-	}
+        imageContainer.classList.add("loaded");
+    }
 
     function addImagesToRows() {
-    	var i,
-			images = document.body.getElementsByClassName('gallery__image-container'),
-			rows = [
-				[]
-			],
-			currentRow = 0;
+        var i,
+            images = document.body.getElementsByClassName('gallery__image-container'),
+            rows = [
+                []
+            ],
+            currentRow = 0;
 
-		for (i = images.length - 1; i >= 0; i--) {
-			if (rows[0].length === galleryProps.maxImagesPerRow) {
-				rows.unshift([]);
-			}
+        for (i = images.length - 1; i >= 0; i--) {
+            if (rows[0].length === galleryProps.maxImagesPerRow) {
+                rows.unshift([]);
+            }
 
-			rows[0].unshift(images[i]);
+            rows[0].unshift(images[i]);
 
-			if (i === 0 && rows[0].length < galleryProps.minImagesPerRow && rows.length > 1) {
-				rows[0].push(rows[1].shift());
-			} 
-		}
+            if (i === 0 && rows[0].length < galleryProps.minImagesPerRow && rows.length > 1) {
+                rows[0].push(rows[1].shift());
+            }
+        }
 
-		galleryRows = rows;
+        galleryRows = rows;
     }
 
     function addFlexClass() {
-    	var i, j;
+        var i, j;
 
-    	for (i = 0; i < galleryRows.length; i++) {
-			for (j = 0; j < galleryRows[i].length; j++) {
-				galleryRows[i][j].classList.add("gallery__image-container--flex" + galleryRows[i].length);
-			}
-		}
-	}
-
-    function adjustHeights() {
-    	var i, j, rowHeights = [];
-
-		for (i = 0; i < galleryRows.length; i++) {
-			for (j = 0; j < galleryRows[i].length; j++) {
-				if (!rowHeights[i]) {
-					rowHeights.push(galleryRows[i][j].offsetWidth);
-				}
-				galleryRows[i][j].style.height = rowHeights[i] + "px";
-			}
-		}
+        for (i = 0; i < galleryRows.length; i++) {
+            for (j = 0; j < galleryRows[i].length; j++) {
+                galleryRows[i][j].classList.add("gallery__image-container--flex" + galleryRows[i].length);
+            }
+        }
     }
 
-	return {
+    function adjustHeights() {
+        var i, j, rowHeights = [];
+
+        for (i = 0; i < galleryRows.length; i++) {
+            for (j = 0; j < galleryRows[i].length; j++) {
+                if (!rowHeights[i]) {
+                    rowHeights.push(galleryRows[i][j].offsetWidth);
+                }
+                galleryRows[i][j].style.height = rowHeights[i] + "px";
+            }
+        }
+    }
+
+    return {
         init: init
     };
 });
